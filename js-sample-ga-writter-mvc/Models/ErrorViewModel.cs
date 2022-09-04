301e02301f1da3dc26e1852ada3c3b1b511abfb8
AndriiHryhorenko <hryhorenkoandriimail@gmail.com>
2022-09-04 16:02:23 +0200

    Initial commit
    js-samle-mvc

---
+++ js-sample-ga-writter-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace js_sample_ga_writter_mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
